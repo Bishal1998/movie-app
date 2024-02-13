406bdd9bd2754346b53aba92d42c3102aa9bdcfc
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-02-13 11:17:01 +0545

    Pages and React router setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Footer, Header } from "./components";
+import { Home, Login, Register } from "./pages";
 
-const App = () => {
-  return <div className="text-orange-600">Movie App</div>;
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 };
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {},
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+const App = () => <RouterProvider router={router} />;
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,24 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />} />)
-);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <App />
     </Provider>
   </React.StrictMode>
 );
