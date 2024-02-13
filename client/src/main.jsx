6b91201c99d35f0041fbedea5bf09f995a0ccdd3
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-02-13 09:41:13 +0545

    main file setup with router and redux provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />} />)
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
