e6d55b0eda8ba44aad60144d3c8a53f45c7f8365
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-02-17 18:06:38 +0545

    movieUpdated

--- server/controller/movieController.js
+++ server/controller/movieController.js
@@ -28,7 +28,30 @@ const getAllMovie = asyncHandler(async (req, res) => {
 })
 
 const updateMovie = asyncHandler(async (req, res) => {
-    console.log("Update Movie")
+    const { id } = req.params;
+
+    const movieExists = await Movie.findById(id);
+
+    if (!movieExists) {
+        return res.status(400).json({ message: "Movie does not exist" });
+    }
+
+    const { title, description, videoUrl, thumbnailUrl, genre, duration } = req.body;
+
+    try {
+        movieExists.title = title || movieExists.title,
+            movieExists.description = description || movieExists.description,
+            movieExists.videoUrl = videoUrl || movieExists.videoUrl,
+            movieExists.thumbnailUrl = thumbnailUrl || movieExists.thumbnailUrl,
+            movieExists.genre = genre || movieExists.genre,
+            movieExists.duration = duration || movieExists.duration
+
+        const updatedMovie = await movieExists.save();
+        res.status(200).json(updatedMovie);
+    } catch (error) {
+        throw new Error("Could not update the movie")
+    }
+
 })
 
 const deleteMovie = asyncHandler(async (req, res) => {
