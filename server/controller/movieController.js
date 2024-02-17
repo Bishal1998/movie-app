f501cad6f178af4722e686513b98321dbbe0f31c
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-02-17 17:45:15 +0545

    movie created successfully

--- server/controller/movieController.js
+++ server/controller/movieController.js
@@ -1,7 +1,24 @@
 import asyncHandler from "../middleware/asyncHandler.js";
+import Movie from "../models/movieModels.js";
 
 const createMovie = asyncHandler(async (req, res) => {
-    console.log("Create Movie")
+
+    const { title, description, videoUrl, thumbnailUrl, genre, duration } = req.body;
+
+    if (!title || !description || !videoUrl || !thumbnailUrl || !genre || !duration) {
+        throw new Error("All Fields are required.");
+    }
+
+    const slug = title.split(" ").join("-").toLowerCase();
+
+    const newMovie = new Movie({ title, description, videoUrl, thumbnailUrl, genre, duration, slug, userId: req.user._id })
+    try {
+        await newMovie.save();
+        res.status(201).json(newMovie)
+    } catch (error) {
+        res.status(400);
+        throw new Error("Invalid Movie Data");
+    }
 })
 
 const getAllMovie = asyncHandler(async (req, res) => {
