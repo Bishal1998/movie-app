801876e839b1b7779452773fcd1912639a03a11d
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-02-21 15:54:08 +0545

    Search Functionality added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { useGetAllMoviesQuery } from "../redux/api/movie";
 import { Movie } from "../components";
+import { useSelector } from "react-redux";
 
 const Home = () => {
   const { data } = useGetAllMoviesQuery();
+
+  const { searchTerm } = useSelector((state) => state.search);
+
+  if (!data && !searchTerm)
+    return <p className="text-lg font-bold">No Movies Found</p>;
+
   return (
     <section className="w-full max-w-[1500px] mx-auto py-8 flex">
       <div className="flex flex-col md:flex-row gap-12 justify-center md:justify-between">
-        {data?.map((movie) => {
-          return <Movie key={movie._id} {...movie} />;
-        })}
+        {searchTerm && searchTerm.length > 0
+          ? searchTerm.map((movie) => {
+              return <Movie key={movie._id} {...movie} />;
+            })
+          : data?.map((movie) => {
+              return <Movie key={movie._id} {...movie} />;
+            })}
       </div>
     </section>
   );
